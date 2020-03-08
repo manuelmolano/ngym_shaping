65759687a2bcb6aa29f109911d09902c10841490
martafradera <56633619+martafradera@users.noreply.github.com>
2020-03-08 18:19:26 +0100

    updated according to CV task changes

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ from stable_baselines.common.policies import LstmPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import A2C, ACER  # , PPO2, ACKTR
 num_instances = 9
-main_folder = '/home/molano/CV-Learning/results_1702/'
+main_folder = '/Users/martafradera/CV'
 task = 'CVLearning-v0'
 KWARGS = {'dt': 100,
           'timing': {'fixation': ('constant', 200),
@@ -25,30 +25,29 @@ KWARGS = {'dt': 100,
 algs = [A2C, ACER]  # , PPO2, ACKTR]
 algs_names = ['A2C', 'ACER']  # , 'PPO2', 'ACKTR']
 th_mat = [-1, 0.3, 0.5, 0.7, 0.9]
-window_mat = [100, 500, 1000]
+days_mat = [1, 2, 3]
 for ind_inst in range(num_instances):
     for ind_alg, alg in enumerate(algs):
         for th in th_mat:
             if th == -1:
-                w_mat = [0]
+                d_mat = [0]
             else:
-                w_mat = window_mat
-            for w in w_mat:
+                d_mat = days_mat
+            for d in d_mat:
                 if th == -1:
                     save_folder =\
                         main_folder + '/train_full_' + str(ind_inst) + '_' +\
                         algs_names[ind_alg] + '/'
-                    KWARGS['init_ph'] = 4
                 else:
                     save_folder =\
                         main_folder + 'train_' + str(th) + '_' +\
-                        str(w) + '_' + str(ind_inst) + '_' +\
+                        str(d) + '_' + str(ind_inst) + '_' +\
                         algs_names[ind_alg] + '/'
-                    KWARGS['init_ph'] = 0
                 print(save_folder)
                 if not os.path.exists(save_folder + 'bhvr_data_all.npz'):
                     KWARGS['th_stage'] = th
-                    KWARGS['perf_w_stage'] = w
+                    # KWARGS['perf_w_stage'] = w
+                    KWARGS['days_perf'] = d
                     env = gym.make(task, **KWARGS)
                     env = monitor.Monitor(env, folder=save_folder,
                                           sv_per=10000, sv_fig=False,
