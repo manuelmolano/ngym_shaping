6755692fe717cb725c4b8d82bc13ed79e0387add
manuelmolano <manuelmolanomazon@gmail.com>
2020-04-19 07:11:54 +0200

    minor

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ from stable_baselines.common.policies import LstmPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import A2C  #, ACER  # , PPO2, ACKTR
 num_instances = 9
-main_folder = '/Users/martafradera/CV'
+main_folder = '/home/manuel/CV-Learning/results/tests_bsc/'
 task = 'CVLearning-v0'
 KWARGS = {'dt': 100,
           'timing': {'fixation': ('constant', 200),
