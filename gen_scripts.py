c82e8b54cc9f6284862ff8ffd2b3f3594b495199
manuelmolano <manuelmolanomazon@gmail.com>
2020-04-29 15:47:45 +0200

    save logs in corresponding folder

--- gen_scripts.py
+++ gen_scripts.py
@@ -13,7 +13,6 @@ import importlib
 import itertools
 scriptsdir = '/home/hcli64/hcli64745/shaping/scripts/'
 workdir = '/home/hcli64/hcli64745/'
-logdir = '/home/hcli64/hcli64745/shaping/logs/'
 
 commontxt = (
     'module purge\n'
@@ -55,7 +54,7 @@ def gen_file(exp, n_cpu, run_time, **kwargs):
     with open(f'{scriptsdir}/{exp}/{name}.sh', 'a') as f:
         f.write('#!/bin/sh\n')
         f.write(f'#SBATCH --job-name={name}\n')
-        f.write(f'#SBATCH --output={logdir}{name}.out\n')
+        f.write(f'#SBATCH --output={exp}/log.out\n')
         f.write(f'#SBATCH -D {workdir}\n')
         f.write(f'#SBATCH --cpus-per-task={n_cpu}\n')
         f.write(f'#SBATCH --time={run_time}:00:00\n')
