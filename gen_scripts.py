0af2a880cd0cff2ba5a33c07c8502738a144f71f
manuelmolano <manuelmolanomazon@gmail.com>
2020-05-05 09:41:13 +0200

    minor

--- gen_scripts.py
+++ gen_scripts.py
@@ -13,6 +13,7 @@ import importlib
 import itertools
 scriptsdir = '/home/hcli64/hcli64745/shaping/scripts/'
 workdir = '/home/hcli64/hcli64745/'
+script = 'bsc_run.py'
 
 commontxt = (
     'module purge\n'
@@ -59,7 +60,7 @@ def gen_file(exp, n_cpu, run_time, **kwargs):
         f.write(f'#SBATCH --cpus-per-task={n_cpu}\n')
         f.write(f'#SBATCH --time={run_time}:00:00\n')
         f.write(commontxt)
-        f.write(f'/home/hcli64/hcli64745/shaping_run.py --folder {exp} {cmd}')
+        f.write(f'{workdir}{script}.py --folder {exp} {cmd}')
 
 
 if __name__ == '__main__':
