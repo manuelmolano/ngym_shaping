945b05bf2666aa3c0fbd0b97c9a3482210e6ad01
martafradera <56633619+martafradera@users.noreply.github.com>
2020-05-16 12:16:06 +0200

    z-score, figure, activity matrix

--- get_activity.py
+++ get_activity.py
@@ -6,75 +6,80 @@ Created on Mon Apr  6 09:33:28 2020
 @author: martafradera
 """
 
-import os
+import glob
 import gym
 import neurogym
-import importlib
 import numpy as np
+import matplotlib.pyplot as plt
 from stable_baselines import A2C  # , ACER, PPO2, ACKTR
-from stable_baselines.common.policies import LstmPolicy
-from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
-from stable_baselines.common import set_global_seeds
-from neurogym.wrappers import ALL_WRAPPERS
+from stable_baselines.common.vec_env import DummyVecEnv
+from neurogym.utils import plotting
 
 
 def create_env(task, n_ch, seed):
-    n_cpu = 1
-
     # task
     task_kwargs = {'dt': 100, 'th_stage': 0.75, 'keep_days': 0, 'stages': [4],
                    'n_ch': n_ch, 'timing': {'fixation': ('constant', 300),
                                             'stimulus': ('constant', 500),
-                                            'delay': ('choice', [0, 1000, 3000]),
+                                            'delay': ('choice', [0, 1000,
+                                                                 3000]),
                                             'decision': ('constant', 300)}}
-    # wrappers
-    # wrappers_kwargs = {'Monitor-v0': {'folder': '', 'sv_fig': False,
-    #                                  'sv_per': 10000, 'fig_type': 'svg'}}
-
     env_id = task
     env = gym.make(env_id, **task_kwargs)
     env.seed(seed)
     env = DummyVecEnv([lambda: env])
-    # env = SubprocVecEnv([env for i in range(n_cpu)])  # ???
     return env
 
 
-def load_model(folder, alg):
-    file = folder+'model.zip'
-    model = alg.load(file)
-    return model
+def model_fig(file, folder, protocol, n_ch, data, states):
+    fname = file[file.find(protocol+'_')+len(protocol)+1:file.find('.zip')]
+    fname = folder+protocol+'_n_ch_'+str(n_ch)+'_model_'+fname
+    perf = np.array(data['perf'])
+    perf = perf[np.where(perf != -1)]
+    name = 'protocol: '+protocol+' perf: '+str(round(np.mean(perf), 2))
+    plotting.fig_(data['ob'], data['actions'], gt=data['gt'],
+                  rewards=data['rewards'], states=states,
+                  fname=fname, name=name)
 
 
-def get_activity(folder, alg, n_ch, seed):
-    activity_matrix = np.empty(0)
-    print('creating env')
-    for file in os.listdir(folder):
-        print(file)
-        print('loading model')
-        model = alg.load(folder+file)
-        print('getting activity')
-        env = create_env('CVLearning-v0', n_ch, seed)
-        obs = env.reset()
-        total_obs = obs
-        states_matrix = np.empty(0)
-        for i in range(50):
-                action, _states = model.predict(obs)   # neurons for each stage
-                obs, rewards, dones, info = env.step(action)
-                env.render()
-                states_matrix = np.concatenate((states_matrix, _states[0]),
-                                               axis=0)
-                total_obs = np.concatenate((total_obs, obs))
-        env.close()
-        states_matrix.flatten()
-        activity_matrix = np.concatenate((activity_matrix, states_matrix),
-                                         axis=0)
+def act_fig(activity_mat, folder, protocol, n_ch, **fig_kwargs):
+    f, axes = plt.subplots(1, 1, **fig_kwargs)
+    axes.imshow(activity_mat, aspect='auto', origin='lower')
+    f.savefig(folder+protocol+'_n_ch_'+str(n_ch)+'_activity.png')
+    plt.close(f)
+
+
+def get_activity(folder, alg, protocols, n_ch=2, seed=0, num_steps=1000,
+                 sv_model=False, sv_act=True):
+    for protocol in protocols:
+        files = glob.glob(folder+'model_n_ch_'+str(n_ch)+'_'+protocol+'_*')
+        activity_mat = None
+        for file in files:
+            print(file)
+            model = alg.load(file)
+            env = create_env('CVLearning-v0', n_ch, seed)
+            data = plotting.run_env(env, model=model, num_steps=num_steps)
+            # z-score
+            states_ori = data['states']
+            states = (states_ori - np.mean(states_ori))/np.std(states_ori)
+            # activity mat
+            states_flat = np.transpose(states)
+            states_flat = states_flat.flatten()
+            if activity_mat is None:
+                activity_mat = states_flat
+            else:
+                activity_mat = np.vstack((activity_mat, states_flat))
+            # plotting
+            if sv_model:
+                model_fig(file, folder, protocol, n_ch, data, states)
+        if sv_act:
+            act_fig(activity_mat, folder, protocol, n_ch)
 
 
 if __name__ == "__main__":
-    folder = '/Users/martafradera/Desktop/models/'
+    folder = '/Users/martafradera/Desktop/models/20_ch/'
     alg = A2C
-    n_ch = 2
-    seed = 0
+    protocols = ['01234', '4']
 
-    get_activity(folder, alg, n_ch, seed)
+    get_activity(folder, alg, protocols, n_ch=20, num_steps=1000, sv_model=True)
     # create_env('CVLearning-v0', 2)
