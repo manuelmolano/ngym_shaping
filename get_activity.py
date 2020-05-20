31345f2373dda2eafd800fdaf526d2b4f5b660d7
martafradera <56633619+martafradera@users.noreply.github.com>
2020-05-20 09:23:55 +0200

    activity mat

--- get_activity.py
+++ get_activity.py
@@ -8,7 +8,6 @@ Created on Mon Apr  6 09:33:28 2020
 
 import glob
 import gym
-import neurogym
 import numpy as np
 import matplotlib.pyplot as plt
 from stable_baselines import A2C  # , ACER, PPO2, ACKTR
@@ -31,14 +30,14 @@ def create_env(task, n_ch, seed):
     return env
 
 
-def model_fig(file, folder, protocol, n_ch, data, states):
+def model_fig(file, folder, protocol, n_ch, data):
     fname = file[file.find(protocol+'_')+len(protocol)+1:file.find('.zip')]
     fname = folder+protocol+'_n_ch_'+str(n_ch)+'_model_'+fname
     perf = np.array(data['perf'])
     perf = perf[np.where(perf != -1)]
-    name = 'protocol: '+protocol +' perf: '+str(round(np.mean(perf), 2))
+    name = 'protocol: ' + protocol + ' perf: ' + str(round(np.mean(perf), 2))
     plotting.fig_(data['ob'], data['actions'], gt=data['gt'],
-                  rewards=data['rewards'],  states=states,
+                  rewards=data['rewards'],  states=data['states_'],
                   fname=fname, name=name)
 
 
@@ -66,8 +65,6 @@ def evaluate(env, model, num_steps):
         states_mat.append(states)
         obs, rewards, done, info = env.step(action)
         rew_mat.append(rewards)
-        if len(states) > 0:
-            states_mat.append(states)
         info = info[0]
         if 'gt' in info.keys():
             gt_mat.append(info['gt'])
@@ -76,14 +73,18 @@ def evaluate(env, model, num_steps):
         if info['new_trial']:
             perf.append(info['performance'])
         env.render()
-    states = np.array(states_mat)
-    states = states[:, 0, :]
+    states_ = np.array(states_mat)
+    states_ = states_[:, 0, :]
+    # zscore
+    states_ = (states_ - np.mean(states_))/np.std(states_)
+    fstates = states_[:, int(states_.shape[1]/2):].T
     data = {'ob': ob_mat, 'actions': act_mat, 'gt': gt_mat,
-            'rewards': rew_mat, 'states': states, 'perf': perf}
+            'half_states': fstates, 'rewards': rew_mat, 'states_': states_,
+            'perf': perf}
     return data
 
 
-def get_activity(folder, alg, protocols, n_ch=2, seed=0, num_steps=1000,
+def get_activity(folder, alg, protocols, n_ch=2, seed=1, num_steps=1000,
                  sv_model=False, sv_act=True):
     for protocol in protocols:
         files = glob.glob(folder+'model_n_ch_'+str(n_ch)+'_'+protocol+'_*')
@@ -92,31 +93,38 @@ def get_activity(folder, alg, protocols, n_ch=2, seed=0, num_steps=1000,
             print(file)
             model = alg.load(file)
             env = create_env('CVLearning-v0', n_ch, seed)
-            # data = plotting.run_env(env, model=model, num_steps=num_steps)
             data = evaluate(env, model, num_steps)
-            # z-score
-            states_ori = data['states']
-            states = (states_ori - np.mean(states_ori))/np.std(states_ori)
+            states = data['half_states']
             # activity mat
-            states_flat = np.transpose(states)
-            states_flat = states_flat.flatten()
-            if activity_mat is None:
-                activity_mat = states_flat
-            else:
-                activity_mat = np.vstack((activity_mat, states_flat))
+            perf = np.array(data['perf'])
+            perf = perf[np.where(perf != -1)]
+            perf = round(np.mean(perf), 2)
+            print('perf', perf)
+            if perf > 0.7:
+                if activity_mat is None:
+                    activity_mat = states
+                else:
+                    # activity_mat = np.vstack((activity_mat, states_flat))
+                    activity_mat = np.concatenate((activity_mat, states),
+                                                  axis=0)
+                activity_mat = np.concatenate((activity_mat,
+                                               np.ones((25, num_steps))),
+                                               axis=0)
             # plotting
             if sv_model:
-                model_fig(file, folder, protocol, n_ch, data, states)
+                model_fig(file, folder, protocol, n_ch, data)
         if sv_act:
+            print(np.shape(activity_mat))
             act_fig(activity_mat, folder, protocol, n_ch)
 
 
 if __name__ == "__main__":
-    n_ch = [2, 10, 20]
+    n_ch = [2]  # , 10, 20]
     alg = A2C
     protocols = ['01234', '4']
     for n in n_ch:
         folder = '/Users/martafradera/Desktop/models/'+str(n)+'_ch/'
 
-        get_activity(folder, alg, protocols, n_ch=n, num_steps=1000, sv_model=True)
+        get_activity(folder, alg, protocols, n_ch=n, num_steps=200,
+                     sv_model=True)
     # create_env('CVLearning-v0', 2)
