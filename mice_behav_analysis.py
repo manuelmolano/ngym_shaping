a1e365a905cc2791e77770f8e9be18492423eb07
manuelmolano <manuelmolanomazon@gmail.com>
2021-02-08 09:53:55 +0100

    efficiently plot accuracies colorcoded by stage

--- mice_behav_analysis.py
+++ mice_behav_analysis.py
@@ -67,82 +67,40 @@ def num_sessions_per_stage(df, subj):
     return stg, both
 
 
+def gen_repeating(s):
+    """Generator of groups with repeated elements in an iterable,
+    for example 'abbccc' would be [('a', 0, 0), ('b', 1, 2), ('c', 3, 5)]
+    """
+    i = 0
+    while i < len(s):
+        j = i
+        while j < len(s) and s[j] == s[i]:
+            j += 1
+        yield (s[i], i, j-1)
+        i = j
+
+
 def accuracy_sessions_subj(df, subj):
-#    plt.figure()
-#    col_list = ['#fdd49e', '#fdbb84', '#fc8d59', '#ef6548', '#d7301f',
-#                '#990000']
-    acc = df.loc[df['subject_name'] == subj, 'accuracy'].values
-    stg = df.loc[df['subject_name'] == subj, 'stage_number'].values
-
-
-# create the extremes (a 0 at the beggining and a 1 at the ending)
-    stg = np.insert(stg, 0, 0)
-    stg = np.append(stg, 1)
-    diff = np.diff(stg)
-
-    # TODO: plot chunks: do diff, find changes,
-    # plot chunks from change_t-1 to change_t
-
-    # First trial to do it:
     col_list = ['#fdd49e', '#fdbb84', '#fc8d59', '#ef6548', '#d7301f',
                 '#990000']
-    for i, (a, s) in enumerate(zip(acc, stg)):
-        plt.plot(i+1, a, color=col_list[s])
-        plt.xlabel('Session')
-        plt.ylabel('Accuracy')
-        plt.title('Accuracy across sessions of subject'+' '+subj)
-        plt.show
+    acc = df.loc[df['subject_name'] == subj, 'accuracy'].values
+    stg = df.loc[df['subject_name'] == subj, 'stage_number'].values
+    print(acc)
+    print(stg)
 
-    # 2 trial to do it
-    # all the accuracy parameters of all the subjects
-    df_params['color'] = 'no color'
-    df_params.loc[df_params['stage_number'] == 0, 'color'] = '#fdd49e'
-    df_params.loc[df_params['stage_number'] == 1, 'color'] = '#fdbb84'
-    df_params.loc[df_params['stage_number'] == 2, 'color'] = '#fc8d59'
-    df_params.loc[df_params['stage_number'] == 3, 'color'] = '#ef6548'
-    df_params.loc[df_params['stage_number'] == 4, 'color'] = '#d7301f'
-    df_params.loc[df_params['stage_number'] == 5, 'color'] = '#990000'
-    fig, ax = plt.subplots()
-
-    def gen_repeating(s):
-        """Generator of groups with repeated elements in an iterable,
-        for example 'abbccc' would be [('a', 0, 0), ('b', 1, 2), ('c', 3, 5)]
-        """
-        i = 0
-        while i < len(s):
-            j = i
-            while j < len(s) and s[j] == s[i]:
-                j += 1
-            yield (s[i], i, j-1)
-            i = j
-
-    # Add last lines
-    for color, start, end in gen_repeating(df_params['color']):
-        if start > 0:  # be sure lines are connected
-            start -= 1
-        idx = df_params.index[start:end+1]
-        df_params.loc[idx, 'accuracy'].plot(ax=ax, color=color, label='')
-    plt.show()
-
-    # 3 trial to do it:
-    # Apply labels
-    df_params_28 = df_params.loc[df_params['subject_name'] == 'C28']
-    df_params_28['color'] = 'no color'
-    df_params_28.loc[df_params_28['stage_number'] == 0, 'color'] = '#fdd49e'
-    df_params_28.loc[df_params_28['stage_number'] == 1, 'color'] = '#fdbb84'
-    df_params_28.loc[df_params_28['stage_number'] == 2, 'color'] = '#fc8d59'
-    df_params_28.loc[df_params_28['stage_number'] == 3, 'color'] = '#ef6548'
-    df_params_28.loc[df_params_28['stage_number'] == 4, 'color'] = '#d7301f'
-    df_params_28.loc[df_params_28['stage_number'] == 5, 'color'] = '#990000'
-    fig, ax = plt.subplots()
-
-    # Add last lines
-    for color, start, end in gen_repeating(df_params_28['color']):
-        if start > 0:  # be sure lines are connected
-            start -= 1
-        idx = df_params_28.index[start:end+1]
-        df_params_28.loc[idx, 'accuracy'].plot(ax=ax, color=color, label='')
-    plt.show()
+    # create the extremes (a 0 at the beggining and a 1 at the ending)
+    stg_exp = np.insert(stg, 0, 0)
+    stg_exp = np.append(stg_exp, stg_exp[-1]+1)
+    stg_diff = np.diff(stg_exp)
+    stg_chng = np.where(stg_diff != 0)[0]
+    print(stg_chng)
+    # TODO: plot chunks: do diff, find changes,
+    # plot chunks from change_t-1 to change_t
+    for i_stg in range(1, len(stg_chng)):
+        color = stg_exp[stg_chng[i_stg-1]+1]
+        plt.plot(range(stg_chng[i_stg-1], min(stg_chng[i_stg]+1, len(acc))),
+                 acc[stg_chng[i_stg-1]:min(stg_chng[i_stg]+1, len(acc))],
+                 color=col_list[color-1])
 
 
 if __name__ == '__main__':
