fb99435e1db1b1498f4b81ea5ff47c0478d927bd
manuelmolano <manuelmolanomazon@gmail.com>
2021-02-15 09:17:00 +0100

    improve a bit code and add TODOs

--- mice_behav_analysis.py
+++ mice_behav_analysis.py
@@ -7,7 +7,7 @@ import seaborn as sns
 COLORS = sns.color_palette("mako", n_colors=3)
 
 path = '/Users/leyre/Dropbox/mice_data/standard_training_2020'
-#path = '/home/manuel/mice_data/standard_training_2020'
+path = '/home/manuel/mice_data/standard_training_2020'
 
 
 def plot_xvar_VS_yvar(df, x_var, y_var, col, xlabel='x_var', ylabel='y_var'):
@@ -103,23 +103,39 @@ def accuracy_sessions_subj(df, subj):
     # to ind_t
     acc_list = []
     xs_list = []
+    color_list = []
     for i_stg in range(1, len(stg_chng)):
+        color_list.append(stg_exp[stg_chng[i_stg-1]+1]-1)
         xs = range(stg_chng[i_stg-1], min(stg_chng[i_stg]+1, len(acc)))
         accs = acc[stg_chng[i_stg-1]:min(stg_chng[i_stg]+1, len(acc))]
         acc_list.append(accs)
         xs_list.append(xs)
-    return acc_list, xs_list
+    return acc_list, xs_list, color_list
 
 
-def plot_accuracy_sessions_subj(df, subj, acc, xs, ax):
-    stg = df.loc[df['subject_name'] == subj, 'stage_number'].values
-    stg_exp = np.insert(stg, 0, 0)  # extended stages
-    stg_exp = np.append(stg_exp, stg_exp[-1]+1)
-    stg_diff = np.diff(stg_exp)  # change of stage
-    stg_chng = np.where(stg_diff != 0)[0]  # index where stages change
-    for i_stg in range(1, len(stg_chng)):
+def plot_accuracy_sessions_subj(acc, xs, color, ax):
+    """
+    
+
+    Parameters
+    ----------
+    acc : TYPE
+        DESCRIPTION.
+    xs : TYPE
+        DESCRIPTION.
+    color : TYPE
+        DESCRIPTION.
+    ax : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    # TODO: finish and add docstring
+    for i_chnk, chnk in enumerate(acc):
         # ax_temp.plot([stg_chng[i_stg-1], stg_chng[i_stg-1]], [0, 5], '--k')
-        color = stg_exp[stg_chng[i_stg-1]+1]-1
         ax.plot(xs[i_stg], acc[i_stg], color=COLORS[color])
         ax.axhline(0.5)
         ax.set_title(subj)
@@ -131,7 +147,7 @@ def plot_accuracy_sessions_subj(df, subj, acc, xs, ax):
         ax.set_xlabel('Session')
 
 
-def accuracy_at_stg_change(df):
+def accuracy_at_stg_change(df, subj_unq):
     """The function returns the mean and standard deviation of the changes
     from a stage to another"""
     mat_perfs = {}
@@ -161,22 +177,23 @@ def accuracy_at_stg_change(df):
                 max(0, i_next-len(acc))*[np.nan]
             # add chunk to the dictionary
             mat_perfs[key].append(chunk)
-        mat_mean_perfs = {}
-        mat_std_perfs = {}
-        for key in mat_perfs.keys():
-            assert np.std([len(p) for p in mat_perfs[key]]) == 0
-            mat_mean_perfs[key] = np.nanmean(np.array(mat_perfs[key]))
-            mat_std_perfs[key] = np.nanstd(np.array(mat_perfs[key]))
+    mat_mean_perfs = {}
+    mat_std_perfs = {}
+    for key in mat_perfs.keys():
+        assert np.std([len(p) for p in mat_perfs[key]]) == 0
+        mat_mean_perfs[key] = np.nanmean(np.array(mat_perfs[key]), axis=0)
+        mat_std_perfs[key] = np.nanstd(np.array(mat_perfs[key]), axis=0)
     return mat_mean_perfs, mat_std_perfs
 
 
 def plot_means_sem(means, std):
     plt.close('all')
+    # TODO: for loop going through keys
     m = sorted(means.items())  # sorted by key, return a list of tuples
     x1, y1 = zip(*m)  # unpack a list of pairs into two tuples
     s = sorted(std.items())
     x2, y2 = zip(*s)
-    plt.errorbar(x1, y1, y2, fmt='--o')
+    plt.errorbar(x1, y1, y2, fmt='--o')  # TODO: use axis
     plt.title('Mean and std over stage change')
     plt.xlabel('Stage change')
     plt.ylabel('Accuracy')
@@ -188,7 +205,7 @@ if __name__ == '__main__':
     df_params = pd.read_csv(path + '/global_params.csv', sep=';')
     fig, ax = plt.subplots(nrows=3, ncols=6)
     ax = ax.flatten()
-#    subj_unq = np.unique(df_params.subject_name)
+    subj_unq = np.unique(df_params.subject_name)
 #    for i_s, sbj in enumerate(subj_unq):
 #        accuracy_sessions_subj(df=df_params, subj=sbj, ax=ax[i_s])
 #    fig.suptitle("Accuracy VS sessions", fontsize="x-large")
@@ -197,14 +214,11 @@ if __name__ == '__main__':
 #               loc="center right",   # Position of legend
 #               borderaxespad=0.1,  # Small spacing around legend box
 #               title='Color legend')
-    for i_s, sbj in enumerate(subj_unq):
-        acc_sbj = accuracy_sessions_subj(df=df_params, subj=sbj)[0]
-        xs_sbj = accuracy_sessions_subj(df=df_params, subj=sbj)[1]
-        plot_accuracy_sessions_subj(df=df_params, subj=sbj, acc=acc_sbj, 
-                                    xs=xs_sbj, ax=ax[i_s])
-    accuracy_at_stg_change(df=df_params)
-    mat_mean_perfs = accuracy_at_stg_change(df_params)[0]
-    mat_std_perfs = accuracy_at_stg_change(df_params)[1]
+    # for i_s, sbj in enumerate(subj_unq):
+    #     acc_sbj, xs_sbj = accuracy_sessions_subj(df=df_params, subj=sbj)
+    #     plot_accuracy_sessions_subj(df=df_params, subj=sbj, acc=acc_sbj, 
+    #                                 xs=xs_sbj, ax=ax[i_s])
+    mat_mean_perfs, mat_std_perfs = accuracy_at_stg_change(df_params, subj_unq)
     plot_means_sem(mat_mean_perfs, mat_std_perfs)
 
     # obtain the list of subjects
