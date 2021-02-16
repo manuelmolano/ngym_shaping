63d7236586f5c82442370dbaa5bd01cd25e6999d
leyreab <azcarateleyre@gmail.com>
2021-02-16 20:09:27 +0100

    problems with hline

--- mice_behav_analysis.py
+++ mice_behav_analysis.py
@@ -3,14 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-import seaborn as sns
 COLORS = sns.color_palette("mako", n_colors=3)
 COLORS_qlt = sns.color_palette("tab10")
 
-# TODO: capital letters
-path = '/Users/leyre/Dropbox/mice_data/standard_training_2020'
-path = '/home/manuel/mice_data/standard_training_2020'
-SV_FOLDER
+PATH = '/Users/leyre/Dropbox/mice_data/standard_training_2020'
+# PATH = '/home/manuel/mice_data/standard_training_2020'
+SV_FOLDER = '/Users/leyre/Dropbox/mice_data/standard_training_2020'
+# SV_FOLDER = '/home/manuel/mice_data/standard_training_2020'
+
 
 def sv_fig(f, name):
     """
@@ -32,7 +32,8 @@ def sv_fig(f, name):
     f.savefig(SV_FOLDER+'/'+name+'.png', dpi=400, bbox_inches='tight')
 
 
-def plot_xvar_VS_yvar(df, x_var, y_var, col, xlabel='x_var', ylabel='y_var'):
+def plot_xvar_VS_yvar(df, x_var, y_var, col, xlabel='x_var', ylabel='y_var',
+                      name='X variable VS Y variable'):
     """
     plot x_var VS y_var.
 
@@ -49,7 +50,7 @@ def plot_xvar_VS_yvar(df, x_var, y_var, col, xlabel='x_var', ylabel='y_var'):
 
     Returns
     -------
-    None.
+    Plot x VS y
 
     """
     f, ax = plt.subplots()
@@ -58,36 +59,7 @@ def plot_xvar_VS_yvar(df, x_var, y_var, col, xlabel='x_var', ylabel='y_var'):
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.show()
-
-
-def accuracy_sessions_subj_old(df, subj, ax):
-    acc = df.loc[df['subject_name'] == subj, 'accuracy'].values
-    stg = df.loc[df['subject_name'] == subj, 'stage_number'].values
-
-    # create the extremes (a 0 at the beggining and a 1 at the ending)
-    stg_exp = np.insert(stg, 0, 0)  # extended stages
-    stg_exp = np.append(stg_exp, stg_exp[-1]+1)
-    stg_diff = np.diff(stg_exp)  # change of stage
-    stg_chng = np.where(stg_diff != 0)[0]  # index where stages change
-    # _, ax_temp = plt.subplots(nrows=1, ncols=1)
-    # ax_temp.plot(stg_exp, label='stage')
-    # ax_temp.plot(stg_diff, label='stage diff')
-    # We go over indexes where stage changes and plot chunks from ind_t-1
-    # to ind_t
-    for i_stg in range(1, len(stg_chng)):
-        # ax_temp.plot([stg_chng[i_stg-1], stg_chng[i_stg-1]], [0, 5], '--k')
-        color = stg_exp[stg_chng[i_stg-1]+1]-1
-        xs = range(stg_chng[i_stg-1], min(stg_chng[i_stg]+1, len(acc)))
-        accs = acc[stg_chng[i_stg-1]:min(stg_chng[i_stg]+1, len(acc))]
-        ax.plot(xs, accs, color=COLORS[color])
-        ax.axhline(0.5)
-        ax.set_title(subj)
-        ax.set_ylim(0.4, 1)
-    if subj == 'N01' or subj == 'N07' or subj == 'N13':
-        ax.set_ylabel('Accuracy')
-    if subj == 'N13' or subj == 'N14' or subj == 'N15' or\
-       subj == 'N16' or subj == 'N17' or subj == 'N18':
-        ax.set_xlabel('Session')
+    sv_fig(f, name)
 
 
 def accuracy_sessions_subj(df, subj):
@@ -130,7 +102,7 @@ def accuracy_sessions_subj(df, subj):
     return acc_list, xs_list, color_list
 
 
-def plot_accuracy_sessions_subj(acc, xs, color, ax, subj):
+def plot_accuracy_sessions_subj(acc, xs, col, ax, subj):
     """
     The function plots accuracy over session for every subject, showing
     the stages the mice are in different colors.
@@ -154,17 +126,13 @@ def plot_accuracy_sessions_subj(acc, xs, color, ax, subj):
     The plot of accuracy over session for every subject.
 
     """
-    # TODO: finish
     for i_chnk, chnk in enumerate(acc):
-        ax.plot(xs[i_chnk], acc[i_chnk], color=COLORS[color[i_chnk]])
-        ax.axhline(0.5)
+        ax.plot(xs[i_chnk], acc[i_chnk], color=COLORS[col[i_chnk]])
         ax.set_title(subj)
         ax.set_ylim(0.4, 1)
     if subj in ['N01', 'N07', 'N13']:
         ax.set_ylabel('Accuracy')
-    # TODO: do as above
-    if subj == 'N13' or subj == 'N14' or subj == 'N15' or\
-       subj == 'N16' or subj == 'N17' or subj == 'N18':
+    if subj in ['N13', 'N14', 'N15', 'N16', 'N17', 'N18']:
         ax.set_xlabel('Session')
 
 
@@ -212,17 +180,18 @@ def accuracy_at_stg_change(df, subj_unq, prev_w=10, nxt_w=10):
             mat_perfs[key].append(chunk)
     mat_mean_perfs = {}
     mat_std_perfs = {}
-    # TODO: save and show number of samples
+    number_samples = []
     for key in mat_perfs.keys():
+        number_samples.append(len(mat_perfs[key]))  # save number of samples
         assert np.std([len(p) for p in mat_perfs[key]]) == 0
         mat_mean_perfs[key] = np.nanmean(np.array(mat_perfs[key]), axis=0)
         sqrt_n_smpls = np.sqrt(np.array(mat_perfs[key]).shape[0])
         mat_std_perfs[key] =\
             np.nanstd(np.array(mat_perfs[key]), axis=0)/sqrt_n_smpls
-    return mat_mean_perfs, mat_std_perfs
+    return mat_mean_perfs, mat_std_perfs, number_samples
 
 
-def plot_means_std(means, std, prev_w=10, nxt_w=10):
+def plot_means_std(means, std, list_samples, prev_w=10, nxt_w=10):
     """
     Plot mean and standard deviation from the accuracies of each state of each
     mouse.
@@ -248,33 +217,74 @@ def plot_means_std(means, std, prev_w=10, nxt_w=10):
     for i_k, (key, val) in enumerate(means.items()):
         ax[i_k].errorbar(xs, val, std[key], label=key)
         ax[i_k].set_ylim(0.5, 1)
-        ax[i_k].set_title(key)
-        ax[i_k].set_xlabel('Session')
+        ax[i_k].set_title(key + ' (N='+str(list_samples[i_k])+')')
+        ax[i_k].set_ylabel('Mean accuracy')
+        ax[i_k].set_xlabel('Sessions after stage change')
         ax[i_k].axvline(0, color='black', linestyle='--')
+    sv_fig(fig, 'Mean Accuracy of changes')
+
+
+def concatenate_trials_sub(df, subj_unq):
+    """
+    Concatenates for each subject all hithistory variables (true/false),
+    which describe the success of the trial.
+
+    Parameters
+    ----------
+    df : dataframe
+        dictionary containing all the stage changes (e.g. '1-2', '2-3'...) and
+        the accuracies associated to each change.
+    subj_unq : numpy.ndarray
+        array of strings with the name of all the subjects
+
+    Returns
+    -------
+    A dictionary where the keys are the subjects and the values are the 445883
+    hithistories. For each trial, there is a hithistory associated to a subject
+
+    """
+    hithistory_mat = df.hithistory
+    subject_name_mat = df.subject_name
+    hithistory_subj_vectors = {}
+    for i_s, sbj in enumerate(subj_unq):
+        key = str(sbj)
+        if key not in hithistory_subj_vectors.keys():
+            hithistory_subj_vectors[key] = []
+        for index, subjects in enumerate(subject_name_mat):
+            hithistory_subj_vectors[key].append(hithistory_mat[index])
+    return hithistory_subj_vectors
 
 
 if __name__ == '__main__':
     plt.close('all')
-    df_params = pd.read_csv(path + '/global_params.csv', sep=';')
+    df_params = pd.read_csv(PATH + '/global_params.csv', sep=';')
     subj_unq = np.unique(df_params.subject_name)
-    # plot acc_sess todo junto que funciona
     fig, ax = plt.subplots(nrows=3, ncols=6)
     ax = ax.flatten()
     for i_s, sbj in enumerate(subj_unq):
         acc_sbj, xs_sbj, color_sbj = accuracy_sessions_subj(df=df_params,
                                                             subj=sbj)
-        plot_accuracy_sessions_subj(acc=acc_sbj, xs=xs_sbj, color=color_sbj,
+        plot_accuracy_sessions_subj(acc=acc_sbj, xs=xs_sbj, col=color_sbj,
                                     ax=ax[i_s], subj=sbj)
-    # TODO: legend, horizontal line dashed and black
+    fig.suptitle("Accuracy VS sessions", fontsize="x-large")
+    fig.legend([COLORS[0], COLORS[1], COLORS[2]],
+               labels=['Stage 1', 'Stage 2', 'Stage 3'],
+               loc="center right",   # Position of legend
+               borderaxespad=0.1,  # Small spacing around legend box
+               title='Color legend')
+    sv_fig(fig, 'Accuracy VS sessions')
     # performance at stage change
     prev_w = 10
     nxt_w = 10
-    mat_mean_perfs, mat_std_perfs = accuracy_at_stg_change(df_params, subj_unq,
-                                                           prev_w=prev_w,
-                                                           nxt_w=nxt_w)
-    plot_means_std(mat_mean_perfs, mat_std_perfs, prev_w=prev_w, nxt_w=nxt_w)
+    mat_mean_perfs, mat_std_perfs, num_samples =\
+        accuracy_at_stg_change(df_params, subj_unq, prev_w=prev_w, nxt_w=nxt_w)
+    plot_means_std(mat_mean_perfs, mat_std_perfs, num_samples, prev_w=prev_w,
+                   nxt_w=nxt_w)
+    # concatenate all trials for each subject
+    plt.close('all')
+    df_trials = pd.read_csv(PATH + '/global_trials.csv', sep=';',
+                            low_memory=False)  # to avoid NAN problems
+    hithistory_subj_vectors = concatenate_trials_sub(df_trials, subj_unq)
+    hithistory_subj_vectors.keys()
+    hithistory_subj_vectors['N11']
 
-    # obtain the list of subjects
-#     subj_mat = df_params.subject_name
-#     subj_unq = np.unique(subj_mat)
-#     print(subj_unq)
