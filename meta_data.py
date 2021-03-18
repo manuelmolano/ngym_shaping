99792993ece727a956f28c9316cf9bc026dd61f1
manuelmolano <manuelmolanomazon@gmail.com>
2021-03-18 08:59:41 +0100

    create meta_data file

---
+++ meta_data.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 18 08:58:14 2021
+
+@author: manuel
+"""
+
+events_dict = {'N01': {'lick_teach': 3, 'surgery': '-'},
+               'N02': {'lick_teach': 4, 'surgery': '03022020'}}
