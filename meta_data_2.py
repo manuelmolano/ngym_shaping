eae8283f3841cec3912834f0d068055195dec38a
leyreab <64582366+leyreab@users.noreply.github.com>
2021-03-25 16:33:04 +0100

    Add files via upload

---
+++ meta_data_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 18 08:58:14 2021
+
+@author: manuel
+"""
+
+events_dict = {'C17': {'lick_teach': 5, 'surgery': '-',
+                       'sick': '-', 'wounds': '-'},
+               'C18': {'lick_teach': 5, 'surgery': '-',
+                       'sick': '-', 'wounds': '-'},
+               'C19': {'lick_teach': 3, 'surgery': '-',
+                       'sick': '-', 'wounds': '-'},
+               'C20': {'lick_teach': 4, 'surgery': '-',
+                       'sick': '-', 'wounds': '-'},
+               'C21': {'lick_teach': 4, 'surgery': '-',
+                       'sick': '-', 'wounds': '-'},
+               'C22': {'lick_teach': 4, 'surgery': '-',
+                       'sick': '-', 'wounds': '-'}}
