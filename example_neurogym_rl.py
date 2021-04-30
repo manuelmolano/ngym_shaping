a287c040691045498dab2fa7a94f5a181687dff8
manuelmolano <manuelmolanomazon@gmail.com>
2021-04-30 10:11:28 +0200

    plot general fig, add comments

--- example_neurogym_rl.py
+++ example_neurogym_rl.py
@@ -2,20 +2,24 @@
 
 
 import numpy as np
+import os
 import gym
 import matplotlib.pyplot as plt
 import ngym_shaping as ng_sh
+from ngym_shaping.utils import plotting as plot
 import warnings
 # from stable_baselines.common.policies import LstmPolicy
 # from stable_baselines.common.vec_env import DummyVecEnv
 # from stable_baselines import A2C  # ACER, PPO2
 warnings.filterwarnings('default')
+rerun = True
 learn = False
-punishment_variation = False
-probes = True
-# sv_f = '/home/molano/shaping/results_280421/'
+punishment_variation = True
+probes = False
+plot_figs = True
+sv_f = '/home/molano/shaping/results_280421/'
 # sv_f = '/home/manuel/shaping/results_280421/'
-sv_f = '/Users/leyreazcarate/Desktop/TFG/shaping/'
+# sv_f = '/Users/leyreazcarate/Desktop/TFG/shaping/'
 num_steps = 200000  # 1e5*np.arange(10, 21, 2)
 num_instances = 3
 mean_perf = []
@@ -23,71 +27,24 @@ stages = np.arange(5)
 th = 0.75
 perf_w = 100
 stg_w = 1000
+conv_w = 50
+rand_act_prob = 0.01
 timing = {'fixation': ('constant', 0),
           'stimulus': ('constant', 300),
           'delay': (0, 100, 300),
           'decision': ('constant', 200)}
 rewards = {'abort': -0.1, 'correct': +1., 'fail': -0.1}
 env_kwargs = {'timing': timing, 'rewards': rewards}
-for ind in range(num_instances):
-    sv_f_inst = sv_f+'/instance_'+str(ind)+'/'
-    env = ng_sh.envs.DR_stage.shaping(stages=stages, th=th, perf_w=perf_w,
-                                      stg_w=stg_w, sv_folder=sv_f_inst,
-                                      sv_per=stg_w, **env_kwargs)
-    if learn:
-        env = DummyVecEnv([lambda: env])
-        # Define model
-        model = A2C(LstmPolicy, env, verbose=1,
-                    policy_kwargs={'feature_extraction': "mlp"})
-        # Train model
-        model.learn(total_timesteps=num_steps, log_interval=10e10)
-    else:
-        env.reset()
-        for ind in range(100000):
-            action = env.gt_now  # correct action (gt means ground-truth)
-            env.step(action)
-    env.close()
-    # env = ng_sh.envs.DR_stage.shaping(stages=[4], th=th, perf_w=perf_w,
-    #                                   stg_w=stg_w, **env_kwargs)
-    # env = DummyVecEnv([lambda: env])
-    # ng_sh.utils.plotting.plot_env(env, num_trials=100, model=model)
-    # data = ng_sh.utils.plotting.run_env(env, num_trials=1000, model=model)
-    # perf = np.array(data['perf'])
-    # mean_perf.append(np.mean(perf[perf != -1.]))
-
-    # print(mean_perf)
-    
-### Plot real performance, mean performance and stages
-    conv_w = 50
-    plt.close('all')
-    f, ax = plt.subplots(nrows=2, sharex=True)
-    ng_sh.utils.plotting.plot_rew_across_training(folder=sv_f_inst, ax=ax[0],
-                                                  fkwargs={'c': 'tab:red'},
-                                                  legend=False, zline=False,
-                                                  metric_name='performance',
-                                                  window=conv_w)
-    ng_sh.utils.plotting.plot_rew_across_training(folder=sv_f_inst, ax=ax[0],
-                                                  fkwargs={'c': 'tab:blue'},
-                                                  legend=False, zline=False,
-                                                  metric_name='real_performance',
-                                                  window=conv_w)
-    ax[0].axhline(y=th, linestyle='--', color='k')
-    ng_sh.utils.plotting.plot_rew_across_training(folder=sv_f_inst, ax=ax[1],
-                                                  fkwargs={'c': 'tab:blue'},
-                                                  legend=False, zline=False,
-                                                  metric_name='stage',
-                                                  window=conv_w)
-    f.savefig(sv_f_inst+'training.png', dpi=300)
 
 ### Plot real performance, mean performance and stages with different punishments
 # plot of each punishment and each instance
-if punishment_variation:
-    plt.close('all')
-    for ind in range(num_instances):
-        sv_f_inst = sv_f+'/instance_'+str(ind)+'/'
-        for i in np.arange(-0.5, 0, 0.1):
-            i = round(i, 2)
-            rewards = {'abort': -0.1, 'correct': +1., 'fail': i}
+plt.close('all')
+for i_i in range(num_instances):
+    for pun in np.linspace(-0.5, 0, 3):  # TODO: define pun mat
+        sv_f_inst = sv_f+'/pun_'+str(round(pun, 2))+'_inst_'+str(i_i)+'/'
+        print(sv_f_inst)
+        if not os.path.exists(sv_f_inst+'/bhvr_data_all.npz') or rerun:
+            rewards = {'abort': -0.1, 'correct': +1., 'fail': pun}
             env_kwargs = {'timing': timing, 'rewards': rewards}
             env = ng_sh.envs.DR_stage.shaping(stages=stages, th=th, perf_w=perf_w,
                                               stg_w=stg_w, sv_folder=sv_f_inst,
@@ -101,85 +58,55 @@ if punishment_variation:
                 model.learn(total_timesteps=num_steps, log_interval=10e10)
             else:
                 env.reset()
-                for ind in range(100000):
-                    action = env.gt_now  # correct action (gt means ground-truth)
+                for ind in range(100000):  # TODO: define
+                    if np.random.rand() < (rand_act_prob-pun):
+                        action = np.random.randint(0, 3)
+                    else:
+                        action = env.gt_now  # correct action (gt = ground-truth)
                     env.step(action)
             env.close()
-            conv_w = 50
             f, ax = plt.subplots(nrows=2, sharex=True)
-            ax[0].set_title('Punishment' + str(i))
-            ng_sh.utils.plotting.plot_rew_across_training(folder=sv_f_inst, ax=ax[0],
-                                                          fkwargs={'c': 'tab:red'},
-                                                          legend=False, zline=False,
-                                                          metric_name='performance',
-                                                          window=conv_w)
-            ng_sh.utils.plotting.plot_rew_across_training(folder=sv_f_inst, ax=ax[0],
-                                                          fkwargs={'c': 'tab:blue'},
-                                                          legend=False, zline=False,
-                                                          metric_name='real_performance',
-                                                          window=conv_w)
+            ax[0].set_title('Punishment' + str(round(pun, 2)))
+            plot.plot_rew_across_training(folder=sv_f_inst, ax=ax[0],
+                                          fkwargs={'c': 'tab:red'},
+                                          legend=False, zline=False,
+                                          metric_name='performance',
+                                          window=conv_w)
+            plot.plot_rew_across_training(folder=sv_f_inst, ax=ax[0],
+                                          fkwargs={'c': 'tab:blue'},
+                                          legend=False, zline=False,
+                                          metric_name='real_performance',
+                                          window=conv_w)
             ax[0].axhline(y=th, linestyle='--', color='k')
-            ng_sh.utils.plotting.plot_rew_across_training(folder=sv_f_inst, ax=ax[1],
-                                                          fkwargs={'c': 'tab:blue'},
-                                                          legend=False, zline=False,
-                                                          metric_name='stage',
-                                                          window=conv_w)
-            f.savefig(sv_f_inst+'training with punishment' + str(i) +'.png', dpi=300)
-                
-        
-if probes:
+            plot.plot_rew_across_training(folder=sv_f_inst, ax=ax[1],
+                                          fkwargs={'c': 'tab:blue'},
+                                          legend=False, zline=False,
+                                          metric_name='stage',
+                                          window=conv_w)
+            f.savefig(sv_f_inst+'pun_'+str(round(pun, 2))+'.png', dpi=300)
+if plot_figs:
     plt.close('all')
-    for ind in range(num_instances):
-        sv_f_inst = sv_f+'/instance_'+str(ind)+'/'
-        for index, punishment in enumerate(np.arange(-0.5, 0, 0.1)):
-            punishment = round(punishment, 2)
-            rewards = {'abort': -0.1, 'correct': +1., 'fail': punishment}
-            env_kwargs = {'timing': timing, 'rewards': rewards}
-            env = ng_sh.envs.DR_stage.shaping(stages=stages, th=th, perf_w=perf_w,
-                                              stg_w=stg_w, sv_folder=sv_f_inst,
-                                              sv_per=stg_w, **env_kwargs)
-            if learn:
-                env = DummyVecEnv([lambda: env])
-                # Define model
-                model = A2C(LstmPolicy, env, verbose=1,
-                            policy_kwargs={'feature_extraction': "mlp"})
-                # Train model
-                model.learn(total_timesteps=num_steps, log_interval=10e10)
-            else:
-                env.reset()
-                for ind in range(100000):
-                    action = env.gt_now  # correct action (gt means ground-truth)
-                    env.step(action)
-            env.close()
-            conv_w = 50
-            f, ax = plt.subplots(nrows=2, ncols=5, sharex=True)
-            cols = ['Punishment {}'.format(round(col,2)) for col in np.arange(-0.5, 0, 0.1)]
-            rows = ['Performance', 'Stages']
-            for axes, col in zip(ax[0], cols):
-                axes.set_title(col)
-            for axes, row in zip(ax[:,0], rows):
-                axes.set_ylabel(row, rotation=0, size='large')
-            f.tight_layout()
-            # ax.set_title('Punishment' + str(punishment))
-            ng_sh.utils.plotting.plot_rew_across_training(folder=sv_f_inst, ax=ax[0:index],
-                                                          fkwargs={'c': 'tab:red'},
-                                                          legend=False, zline=False,
-                                                          metric_name='performance',
-                                                          window=conv_w)
-            ng_sh.utils.plotting.plot_rew_across_training(folder=sv_f_inst, ax=ax[0:index],
-                                                          fkwargs={'c': 'tab:blue'},
-                                                          legend=False, zline=False,
-                                                          metric_name='real_performance',
-                                                          window=conv_w)
+    f, ax = plt.subplots(nrows=2, ncols=1, sharex=True)
+    fkwargs = {'c': 'tab:blue'}
+    for i_p, pun in enumerate(np.linspace(-0.5, 0, 6)):  # TODO: define pun mat
+        for i_i in range(num_instances):
+            pun_str = str(round(pun, 2))
+            sv_f_inst = sv_f+'/pun_'+pun_str+'_inst_'+str(i_i)+'/'
+            fkwargs['alpha'] = 1-1/(i_p+2)
+            fkwargs['label'] = 'pun = '+pun_str if i_i == 0 else ''
+            plot.plot_rew_across_training(folder=sv_f_inst, ax=ax[0],
+                                          fkwargs=fkwargs, legend=False,
+                                          zline=False, window=conv_w,
+                                          metric_name='real_performance')
             ax[0].axhline(y=th, linestyle='--', color='k')
-            ng_sh.utils.plotting.plot_rew_across_training(folder=sv_f_inst, ax=ax[1:index],
-                                                          fkwargs={'c': 'tab:blue'},
-                                                          legend=False, zline=False,
-                                                          metric_name='stage',
-                                                          window=conv_w)
-            f.savefig(sv_f_inst+'training with punishment' + str(punishment) +'.png', dpi=300)
-                
-    
+            plot.plot_rew_across_training(folder=sv_f_inst, ax=ax[1],
+                                          fkwargs=fkwargs, legend=False,
+                                          zline=False, window=conv_w,
+                                          metric_name='stage')
+    f.tight_layout()
+    ax[0].legend()
+    f.savefig(sv_f+'/pun_'+pun_str+'_all_insts.png', dpi=300)
 
 
-
+if __name__ == '__main__':
+    print('separate code into functions')  # TODO: do
