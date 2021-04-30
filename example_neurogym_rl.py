757092fa805a3f954b5dda7faf899263ff479d74
manuelmolano <manuelmolanomazon@gmail.com>
2021-04-30 16:13:32 +0200

    make some global vars

--- example_neurogym_rl.py
+++ example_neurogym_rl.py
@@ -8,37 +8,37 @@ import matplotlib.pyplot as plt
 import ngym_shaping as ng_sh
 from ngym_shaping.utils import plotting as plot
 import warnings
-# from stable_baselines.common.policies import LstmPolicy
-# from stable_baselines.common.vec_env import DummyVecEnv
-# from stable_baselines import A2C  # ACER, PPO2
+from stable_baselines.common.policies import LstmPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import A2C  # ACER, PPO2
 warnings.filterwarnings('default')
 
 
 # plot of each punishment and each instance
-def learning(num_instances, punish_3_vector, sv_f, stages, th, perf_w, stg_w,
+def learning(num_instances, punish_3_vector, sv_f, stages, perf_w, stg_w,
              env_kwargs):
     for i_i in range(num_instances):
         for pun in punish_3_vector:
             sv_f_inst = sv_f+'/pun_'+str(round(pun, 2))+'_inst_'+str(i_i)+'/'
             print(sv_f_inst)
-            if not os.path.exists(sv_f_inst+'/bhvr_data_all.npz') or rerun:
+            if not os.path.exists(sv_f_inst+'/bhvr_data_all.npz') or RERUN:
                 rewards = {'abort': -0.1, 'correct': +1., 'fail': pun}
-                env_kwargs = {'timing': timing, 'rewards': rewards}
-                env = ng_sh.envs.DR_stage.shaping(stages=stages, th=th,
+                env_kwargs['rewards'] = rewards
+                env = ng_sh.envs.DR_stage.shaping(stages=stages, th=TH,
                                                   perf_w=perf_w,
                                                   stg_w=stg_w,
                                                   sv_folder=sv_f_inst,
                                                   sv_per=stg_w, **env_kwargs)
-                if learn:
+                if LEARN:
                     env = DummyVecEnv([lambda: env])
                     # Define model
                     model = A2C(LstmPolicy, env, verbose=1,
                                 policy_kwargs={'feature_extraction': "mlp"})
                     # Train model
-                    model.learn(total_timesteps=num_steps, log_interval=10e10)
+                    model.learn(total_timesteps=NUM_STEPS, log_interval=10e10)
                 else:
                     env.reset()
-                    for ind in range(num_rand):
+                    for ind in range(NUM_RAND):
                         if np.random.rand() < (rand_act_prob-2*pun):
                             action = np.random.randint(0, 3)
                         else:
@@ -48,23 +48,22 @@ def learning(num_instances, punish_3_vector, sv_f, stages, th, perf_w, stg_w,
 
 
 def plot_inst_punishment(num_instances, punish_3_vector, conv_w):
-    plt.close('all')
     for i_i in range(num_instances):
         for pun in punish_3_vector:
             sv_f_inst = sv_f+'/pun_'+str(round(pun, 2))+'_inst_'+str(i_i)+'/'
             f, ax = plt.subplots(nrows=2, sharex=True)
             ax[0].set_title('Punishment' + str(round(pun, 2)))
-            plot.plot_rew_across_training(folder=sv_f_inst, ax=ax[0],
-                                          fkwargs={'c': 'tab:red'},
-                                          legend=False, zline=False,
-                                          metric_name='performance',
-                                          window=conv_w)
+            # plot.plot_rew_across_training(folder=sv_f_inst, ax=ax[0],
+            #                               fkwargs={'c': 'tab:red'},
+            #                               legend=False, zline=False,
+            #                               metric_name='performance',
+            #                               window=conv_w)
             plot.plot_rew_across_training(folder=sv_f_inst, ax=ax[0],
                                           fkwargs={'c': 'tab:blue'},
                                           legend=False, zline=False,
                                           metric_name='real_performance',
                                           window=conv_w)
-            ax[0].axhline(y=th, linestyle='--', color='k')
+            ax[0].axhline(y=TH, linestyle='--', color='k')
             plot.plot_rew_across_training(folder=sv_f_inst, ax=ax[1],
                                           fkwargs={'c': 'tab:blue'},
                                           legend=False, zline=False,
@@ -74,7 +73,6 @@ def plot_inst_punishment(num_instances, punish_3_vector, conv_w):
 
 
 def plot_figs(punish_6_vector, num_instances, conv_w):
-    plt.close('all')
     f, ax = plt.subplots(nrows=2, ncols=1, sharex=True)
     fkwargs = {'c': 'tab:blue'}
     for i_p, pun in enumerate(punish_6_vector):
@@ -88,7 +86,7 @@ def plot_figs(punish_6_vector, num_instances, conv_w):
                                           fkwargs=fkwargs, legend=False,
                                           zline=False, window=conv_w,
                                           metric_name='real_performance')
-            ax[0].axhline(y=th, linestyle='--', color='k')
+            ax[0].axhline(y=TH, linestyle='--', color='k')
             plot.plot_rew_across_training(folder=sv_f_inst, ax=ax[1],
                                           fkwargs=fkwargs, legend=False,
                                           zline=False, window=conv_w,
@@ -99,37 +97,37 @@ def plot_figs(punish_6_vector, num_instances, conv_w):
 
 
 if __name__ == '__main__':
-    rerun = True
-    learn = False
-    plot_separate_figures = True
-    plot_all_figs = False
-    # sv_f = '/home/molano/shaping/results_280421/'
+    plt.close('all')
+    sv_f = '/home/molano/shaping/results_280421/'
     # sv_f = '/home/manuel/shaping/results_280421/'
-    sv_f = '/Users/leyreazcarate/Desktop/TFG/shaping/'
-    num_steps = 200000  # 1e5*np.arange(10, 21, 2)
+    # sv_f = '/Users/leyreazcarate/Desktop/TFG/shaping/'
+    RERUN = False
+    LEARN = True
+    NUM_STEPS = 200000  # 1e5*np.arange(10, 21, 2)
+    TH = 0.75
+    NUM_RAND = 100000
+
+    plot_separate_figures = False
+    plot_all_figs = True
     num_instances = 3
     mean_perf = []
     stages = np.arange(5)
-    th = 0.75
     perf_w = 100
     stg_w = 1000
     conv_w = 50
     rand_act_prob = 0.01
-    num_rand = 100000
     punish_3_vector = np.linspace(-0.5, 0, 3)
-    punish_6_vector = np.linspace(-0.5, 0, 6)
+    punish_6_vector = np.linspace(-0.5, 0, 3)
     timing = {'fixation': ('constant', 0),
               'stimulus': ('constant', 300),
               'delay': (0, 100, 300),
               'decision': ('constant', 200)}
     rewards = {'abort': -0.1, 'correct': +1., 'fail': -0.1}
     env_kwargs = {'timing': timing, 'rewards': rewards}
-    
-    learning(num_instances, punish_3_vector, sv_f, stages, th, perf_w, stg_w,
+    learning(num_instances, punish_3_vector, sv_f, stages, perf_w, stg_w,
              env_kwargs)
     if plot_separate_figures:
         plot_inst_punishment(num_instances, punish_3_vector, conv_w)
     if plot_all_figs:
         plot_figs(punish_6_vector, num_instances, conv_w)
-    
-    print('separate code into functions')  # TODO: do
+    print('separate code into functions')
