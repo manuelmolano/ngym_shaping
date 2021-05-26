756e3de1695d8ea2b140c15a2c0ea2d522425513
manuelmolano <manuelmolanomazon@gmail.com>
2021-05-26 09:42:06 +0200

    allow modifying gamma

--- example_neurogym_rl.py
+++ example_neurogym_rl.py
@@ -15,10 +15,10 @@ warnings.filterwarnings('default')
 
 
 # plot of each punishment and each instance
-def learning(num_instances, punish_3_vector, sv_f, stages, perf_w, stg_w,
-             env_kwargs):
+def learning(sv_f, pun_vector, stages, perf_w=100, stg_w=1000, rollout=5,
+             gamma=0.5, num_instances=3, env_kwargs={}):
     for i_i in range(num_instances):
-        for pun in punish_3_vector:
+        for pun in pun_vector:
             sv_f_inst = sv_f+'/pun_'+str(round(pun, 2))+'_inst_'+str(i_i)+'/'
             print('---------')
             print(sv_f_inst)
@@ -36,7 +36,7 @@ def learning(num_instances, punish_3_vector, sv_f, stages, perf_w, stg_w,
                     # Define model
                     model = A2C(LstmPolicy, env, verbose=1, n_cpu_tf_sess=1,
                                 policy_kwargs={'feature_extraction': "mlp"},
-                                **{'n_steps': 10})
+                                **{'n_steps': rollout, 'gamma': gamma})
                     # Train model
                     model.learn(total_timesteps=NUM_STEPS, log_interval=10e10)
                     model.save(sv_f_inst+'model')
@@ -53,31 +53,36 @@ def learning(num_instances, punish_3_vector, sv_f, stages, perf_w, stg_w,
 
 if __name__ == '__main__':
     plt.close('all')
+    rollout = 5
+    gamma = 0.5
     # sv_f = '/home/molano/shaping/results_280421/no_shaping/'
     # sv_f = '/home/molano/shaping/results_280421/shaping_long_tr_one_agent/'
-    sv_f = '/home/molano/shaping/results_280421/' +\
-        'no_shaping_long_tr_one_agent_stg_4_nsteps_10/'
+    # sv_f = '/home/molano/shaping/results_280421/' +\
+    #     'no_shaping_long_tr_one_agent_stg_4_nsteps_'+str(rollout)+'/'
     # sv_f = '/home/molano/shaping/results_280421/shaping_diff_punishment/'
+    sv_f = '/home/molano/shaping/results_280421/' +\
+        'shaping_'+str(rollout)+'_'+str(gamma)+'/'
+
     RERUN = True
     LEARN = True
     NUM_STEPS = 300000  # 1e5*np.arange(10, 21, 2)
     TH = 0.75
     NUM_RAND = 100000
 
-    num_instances = 3
+    num_instances = 10
     mean_perf = []
-    stages = [4]  # np.arange(4)  #np.array([4])  # np.arange(5)
+    stages = np.arange(2)  # np.array([4])  # np.arange(5)
     perf_w = 100
     stg_w = 1000
     conv_w = 50
     rand_act_prob = 0.01
-    punish_3_vector = np.linspace(-1.0, 0., 5)  # np.linspace(-0.5, 0, 3)
+    pun_vector = np.linspace(-1.0, 0., 5)  # np.linspace(-0.5, 0, 3)
     timing = {'fixation': ('constant', 200),
               'stimulus': ('constant', 400),
               'delay': (0, 1000, 3000),
-              'decision': ('constant', 200)}
-    rewards = {'abort': -0.1, 'correct': +1., 'fail': -0.1}
-    env_kwargs = {'timing': timing, 'rewards': rewards}
-    learning(num_instances, punish_3_vector, sv_f, stages, perf_w, stg_w,
-             env_kwargs)
+              'decision': ('constant', 300)}
+    env_kwargs = {'timing': timing}
+    learning(sv_f=sv_f, num_instances=num_instances, pun_vector=pun_vector,
+             stages=stages, perf_w=perf_w, stg_w=stg_w, rollout=rollout,
+             gamma=gamma, env_kwargs=env_kwargs)
     # TODO: plot performances using analysis_rl.py fns
